67ae8f420d9f3422410a1ca6b29947d2f4aee3ac
allengurney <allen@allengurney.com>
2026-04-25 10:20:32 -0700

    Add related and places site stats

--- site/gurney-eleventy-project_source_clean_2026-04-08_v23/_data/siteStats.js
+++ site/gurney-eleventy-project_source_clean_2026-04-08_v23/_data/siteStats.js
@@ -1,10 +1,15 @@
 const ancestors = require("./ancestors.json");
+const placesCatalog = require("./placesCatalog.json");
 const researchCompanions = require("./researchCompanions.js");
 
 const directAncestors = ancestors.filter(item => item.type === "ancestor");
+const relatedAncestors = ancestors.filter(item => item.type === "related");
 const knownGenerations = directAncestors.filter(item => !/[~+]/.test(String(item.gen || "")));
 const eras = ancestors.filter(item => item.type === "era");
 const places = new Set();
+const placeLinkCount = placesCatalog.reduce((sum, place) => sum + (place.ancestorCount || 0), 0);
+const placeTypes = new Set(placesCatalog.map(place => place.placeType).filter(Boolean));
+const geocodedPlaceCount = placesCatalog.filter(place => place.coordinate && place.coordinate.lat && place.coordinate.lng).length;
 
 directAncestors.forEach(item => {
   (item.placeRefs || []).forEach(placeId => places.add(placeId));
@@ -12,8 +17,12 @@ directAncestors.forEach(item => {
 
 module.exports = {
   directAncestorCount: directAncestors.length,
+  relatedAncestorCount: relatedAncestors.length,
   knownGenerationCount: knownGenerations.length,
   eraCount: eras.length,
-  placeCount: places.size,
+  placeCount: placesCatalog.length || places.size,
+  placeLinkCount,
+  placeTypeCount: placeTypes.size,
+  geocodedPlaceCount,
   researchCompanionCount: researchCompanions.length,
 };
